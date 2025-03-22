370a0b54a65f731b0bb6f6ef41cf18c150fb8095
Brandon Wong <brandonhywong@gmail.com>
2025-03-21 23:58:09 -0400

    Implemented deepspeech wrapper and requirements.txt

---
+++ flask-server/backend/voice_to_text/deepspeech_wrapper.py
@@ -0,0 +1,31 @@
+from deepspeech import Model
+import wave
+import os
+import numpy as np
+
+
+def setup_deepspeech():
+    """Setsup a deepspeech model
+    """
+    # Load the pre-trained model
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct the path to the pbmm file in the same directory
+    model_path = os.path.join(script_directory, 'deepspeech-0.9.3-models.pbmm')
+    return Model(model_path)
+
+
+def transcribe(model: Model, audio_file: str):
+    """Transcribes audio_file to text using model
+
+    Args:
+        model (Model): Deepspeech model
+        audio_file (str): audio file path
+    """
+    # Open audio file
+    with wave.open(audio_file, "r") as audio_file:
+        frames = audio_file.readframes(audio_file.getnframes())
+        audio = np.frombuffer(frames, dtype=np.int16)
+
+    # Run transcription
+    return model.stt(audio)
