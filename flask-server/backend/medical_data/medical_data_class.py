f6525e58289fb0c8e2b349ee6431ab7cce729ab8
Brandon Wong <brandonhywong@gmail.com>
2025-03-22 09:53:20 -0400

    Added init files and fixed some bugs in medial_data_class

--- flask-server/backend/medical_data/medical_data_class.py
+++ flask-server/backend/medical_data/medical_data_class.py
@@ -1,5 +1,6 @@
 from cryptography.fernet import Fernet
 import json
+import pdb
 
 class UserMedicalProfile():
     """Class coantaining relevant user medical history and information 
@@ -9,7 +10,7 @@ class UserMedicalProfile():
     password: str
     name: str
     age: int
-    medical_history: dict[str:str]
+    medical_conditions: dict
     
 
     def __init__(self, user_id: int, username: str, password: str, name: str, age: int, medical_conditions: dict):
@@ -41,7 +42,7 @@ class UserMedicalProfile():
         self.medical_conditions[category] = details
     
     
-    def encrypt_json(self, user_data_path: str, cipher: Fernet):
+    def save_to_data(self, user_data_path: str, cipher: Fernet):
         """Saves UserMedicalProfile in an Encrypted JSON file
         Args:
             user_data_path (str): path of json to store user data
@@ -62,7 +63,7 @@ class UserMedicalProfile():
             json_str = json.dumps(data)
             encrypted_data = cipher.encrypt(json_str.encode())
 
-            with open("encrypted_data.json", "wb") as f:
+            with open(user_data_path, "wb") as f:
                 f.write(encrypted_data)
 
 
@@ -84,6 +85,6 @@ def load_user_from_data(user_data_path: str, cipher: Fernet, username: str, pass
     
     for patient in data:
         if patient["username"] == username and patient["password"] == password:
-            return UserMedicalProfile(patient["user_id"], patient["name"], patient["age"], patient["medical_conditions"])
+            return UserMedicalProfile(patient["user_id"], patient["username"], patient["password"], patient["name"], patient["age"], patient["medical_conditions"])
     return None
-        
+    
