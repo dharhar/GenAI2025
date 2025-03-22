bd70cd76a7cc2ccc61622a5b17c03655b234ce70
Brandon Wong <brandonhywong@gmail.com>
2025-03-22 09:31:31 -0400

    Implemented medical data class and updated requiremements

---
+++ flask-server/backend/medical_data/medical_data_class.py
@@ -0,0 +1,89 @@
+from cryptography.fernet import Fernet
+import json
+
+class UserMedicalProfile():
+    """Class coantaining relevant user medical history and information 
+    """
+    user_id: int
+    username: str
+    password: str
+    name: str
+    age: int
+    medical_history: dict[str:str]
+    
+
+    def __init__(self, user_id: int, username: str, password: str, name: str, age: int, medical_conditions: dict):
+        self.user_id = user_id
+        self.username = username
+        self.password = password
+        self.name = name
+        self.age = age
+        self.medical_conditions = medical_conditions
+    
+    def to_dict(self):
+        data = {
+            "user_id": self.user_id,
+            "username": self.username,
+            "password": self.password,
+            "name": self.name,
+            "age": self.age,
+            "medical_conditions": self.medical_conditions
+        }
+        return data
+    
+    def update_medical_conditions(self, category: str, details: str):
+        """Updates the medical conditions of a user
+
+        Args:
+            category (str): A new category of information (pregnenacy, allergies, etc.)
+            details (str): Details of the patient
+        """
+        self.medical_conditions[category] = details
+    
+    
+    def encrypt_json(self, user_data_path: str, cipher: Fernet):
+        """Saves UserMedicalProfile in an Encrypted JSON file
+        Args:
+            user_data_path (str): path of json to store user data
+            cipher (Fernet): Fernet object for encoding and decoding
+        """
+        
+        with open(user_data_path, "rb") as f:
+            encrypted_data = f.read()
+
+            decrypted_data = cipher.decrypt(encrypted_data).decode()
+            data = json.loads(decrypted_data)  # Convert JSON string to Python dict
+
+            # Append new patient
+            new_patient = self.to_dict()
+            data.append(new_patient)
+
+            # Encrypt and save again
+            json_str = json.dumps(data)
+            encrypted_data = cipher.encrypt(json_str.encode())
+
+            with open("encrypted_data.json", "wb") as f:
+                f.write(encrypted_data)
+
+
+def load_user_from_data(user_data_path: str, cipher: Fernet, username: str, password: str):
+    """Loads a user from user_data.json
+
+    Args:
+        user_data_path (str): path to user data
+        cipher (Fernet): cipher for decoding
+        username (str): users username to lookup
+        password (str): users password to lookup 
+
+    Returns:
+        UserMedicalProfile: user's medical information
+    """
+    with open(user_data_path, "rb") as file:
+        encrypted_data = file.read()
+    data = json.loads(cipher.decrypt(encrypted_data))
+    
+    for patient in data:
+        if patient["username"] == username and patient["password"] == password:
+            return UserMedicalProfile(patient["user_id"], patient["name"], patient["age"], patient["medical_conditions"])
+    return None
+        
